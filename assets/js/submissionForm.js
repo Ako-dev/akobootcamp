477ffe4265aa598e913938125402ba6e10393096
Jeff Yourman <jeffreyyourman@gmail.com>
2020-10-02 13:24:44 -0400

    required fields for firstname and email

--- assets/js/submissionForm.js
+++ assets/js/submissionForm.js
@@ -1,29 +1,35 @@
 // close modal on esc key
 $(document).ready(() => {
-  $('#submitFormBtn').on('click', (e) => {
-    e.preventDefault()
-
-    let form = $('form').serializeArray()
+  $("#submitFormBtn").on("click", (e) => {
+    // e.preventDefault()
+    let form = $("form").serializeArray();
 
     let data = form.reduce((acc, cv) => {
       return {
         ...acc,
         [cv.name]: cv.value,
-      }
-    }, {})
+      };
+    }, {});
 
-    console.log(data)
+    // console.log(data);
 
-
-
-    $.ajax({
-      type: 'POST',
-      crossDomain: true,
-      data: data,
-      url: 'https://tranquil-mesa-15283.herokuapp.com/api/createuser',
-      success: function (jsondata) {
-        console.log(jsondata)
-      }
-    })
-  })
-})
+    if (data.firstName.length < 3 || data.email.length < 3) {
+      // console.log("data.firstName.length", data.firstName.length);
+      // console.log("data.email.length", data.email.length);
+    } else {
+      console.log("inside second if");
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        crossDomain: true,
+        data: data,
+        url: "https://tranquil-mesa-15283.herokuapp.com/api/createuser",
+        success: function (jsondata) {
+          document.getElementById("myForm").reset();
+          console.log('return', jsondata);
+        },
+      });
+      $("#submitFormBtn").attr("data-dismiss", "modal");
+    }
+  });
+});
